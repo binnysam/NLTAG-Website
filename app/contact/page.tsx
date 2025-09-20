469c0d355e574e763bd4dfa8cc3f45be66ff9dc4
binnysam <binnysamuelchristy@gmail.com>
2025-09-20 09:55:11 +0300

    Add nltag-website project

---
+++ app/contact/page.tsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+  return (
+    <div className="mx-auto">
+      <div className="mx-auto max-w-2xl lg:col-span-6 lg:flex lg:items-center justify-center text-center">
+        <div className="relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
+          <div className="flex flex-col items-center">
+            <h1 className="text-foreground text-4xl sm:text-5xl sm:leading-none lg:text-7xl">
+              <span className="block text-foreground">
+                நாங்கள் எங்கள் வாழ்நாளெல்லாம் களிகூர்ந்து மகிழும்படி, காலையிலே
+                எங்களை உமது கிருபையால் திருப்தியாக்கும்.
+              </span>
+              <span className="text-teal-500 block md:ml-0 ">
+                சங்கீதம் 90:14
+              </span>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
