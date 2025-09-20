d08ca18c2084b95488af6d0025af0260d8e6b4e9
binnysam <binnysamuelchristy@gmail.com>
2025-09-20 13:37:10 +0300

    Daily version section - container changes

--- app/verse/page.tsx
+++ app/verse/page.tsx
@@ -1,21 +1,32 @@
 export default function Verse() {
   return (
-    <div className="mx-auto">
-      <div className="mx-auto max-w-2xl lg:col-span-6 lg:flex lg:items-center justify-center text-center">
-        <div className="relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
-          <div className="flex flex-col items-center">
-            <h1 className="text-foreground text-4xl sm:text-5xl sm:leading-none lg:text-7xl">
-              <span className="block text-foreground">
-                நாங்கள் எங்கள் வாழ்நாளெல்லாம் களிகூர்ந்து மகிழும்படி, காலையிலே
-                எங்களை உமது கிருபையால் திருப்தியாக்கும்.
-              </span>
-              <span className="text-teal-500 block md:ml-0 ">
-                சங்கீதம் 90:14
-              </span>
-            </h1>
+    <main className="relative min-h-screen">
+      <div className="relative -mt-[65px]">
+        <div className="sm:py-18 container relative mx-auto px-6 py-16 md:py-24 lg:px-16 lg:py-24 xl:px-20 pt-8 pb-10 md:pt-16 overflow-hidden">
+          <div className="relative">
+            <div className="mx-auto">
+              <div className="mx-auto max-w-2xl lg:col-span-6 lg:flex lg:items-center justify-center text-center">
+                <div className="relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
+                  <div className="flex flex-col items-center">
+                    <h1 className="text-foreground text-4xl sm:text-5xl sm:leading-none lg:text-7xl">
+                      <span className="block text-foreground">
+                        நாங்கள் எங்கள் வாழ்நாளெல்லாம் களிகூர்ந்து மகிழும்படி,
+                        காலையிலே எங்களை உமது கிருபையால் திருப்தியாக்கும்.
+                      </span>
+                      <span className="text-teal-500 block md:ml-0 ">
+                        சங்கீதம் 90:14
+                      </span>
+                      <span className="text-teal-500 block md:ml-0 ">
+                        NLTAG Kuwait, Love You Jesus! 🌸
+                      </span>
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
